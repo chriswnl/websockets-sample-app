a76d395cb91739e1c5666f5f85df8beb001cfedd
Chris Willis <chris@twigtelecom.nl>
2016-10-13 17:58:32 +0200

    Added dependent: :destroy between on chatrooms for messages, and dependent: :nullify on users  to messages. So messages get kept, with 'left user' appearing alongside the message.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  has_many :messages
+  has_many :messages, dependent: :nullify
   has_many :chatrooms, through: :messages
   validates :name, presence: true, uniqueness: true
+  
+  def to_s
+    name
+  end
 end
