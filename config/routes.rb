8a87dd4585f44edf1b5bdd03f943b65ad0658014
Chris Willis <chris@twigtelecom.nl>
2016-10-14 10:56:25 +0200

    Added a couple of styles to sheet and updated a few files.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
     
   resources :chatrooms, param: :slug
   resources :messages
+
+  root to: "chatrooms#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
